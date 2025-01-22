e4c62d62a8cda233774806f5fc90aceac5f18c64
Abhivardhan87699 <abhirathore8555@gmail.com>
2025-01-22 13:53:20 +0530

    Create abhi.py

---
+++ abhi.py
@@ -0,0 +1,28 @@
+a = 5
+print(type(a))
+
+b = 5.0
+print(type(b))
+
+c = 2 + 4j
+print(type(c))
+s = 'Welcome to the Geeks World'
+print(s)
+
+# check data type 
+print(type(s))
+
+# access string with index
+print(s[1])
+print(s[2])
+print(s[0])
+# Empty list
+a = []
+
+# list with int values
+a = [1, 2, 3]
+print(a)
+
+# list with mixed int and string
+b = ["Geeks", "For", "Geeks", 4, 5]
+print(b)
